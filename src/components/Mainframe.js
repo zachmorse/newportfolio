f7e16ef03be72f7e096cd4e59444a54df9d9c0b1
Zach Morse <zacharymorse@gmail.com>
2018-11-15 00:34:19 -0600

    collapsible structure working, placeholder text for now. Tone.js not working yet

---
+++ src/components/Mainframe.js
@@ -0,0 +1,109 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import randomWords from "random-words";
+
+import TonePlayer from "./TonePlayer";
+
+const CollapseContainer = styled.div`
+  display: flex;
+`;
+
+const CollapseBlock = styled.div`
+  background-color: ${props => props.bgColor};
+  width: ${props => (props.expanded ? "calc(100% - 120px)" : "40px")};
+
+  height: 100vh;
+  transition: width 250ms ease-in-out;
+  overflow: scroll;
+  padding: 0rem 1rem;
+`;
+
+export default class MainFrame extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapse1Expanded: true,
+      collapse2Expanded: false,
+      collapse3Expanded: false,
+      collapse4Expanded: false
+    };
+  }
+
+  toggleActive1 = e => {
+    this.setState({
+      collapse1Expanded: true,
+      collapse2Expanded: false,
+      collapse3Expanded: false,
+      collapse4Expanded: false
+    });
+  };
+
+  toggleActive2 = () => {
+    this.setState({
+      collapse1Expanded: false,
+      collapse2Expanded: true,
+      collapse3Expanded: false,
+      collapse4Expanded: false
+    });
+  };
+
+  toggleActive3 = () => {
+    this.setState({
+      collapse1Expanded: false,
+      collapse2Expanded: false,
+      collapse3Expanded: true,
+      collapse4Expanded: false
+    });
+  };
+
+  toggleActive4 = () => {
+    this.setState({
+      collapse1Expanded: false,
+      collapse2Expanded: false,
+      collapse3Expanded: false,
+      collapse4Expanded: true
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <CollapseContainer>
+          <CollapseBlock
+            expanded={this.state.collapse1Expanded}
+            bgColor={"#4bdbfe"}
+            onClick={this.toggleActive1}
+          >
+            <h1>zach morse, a developer in nashville tn</h1>
+            <p>{randomWords({ exactly: 1000, join: " " })}</p>
+          </CollapseBlock>
+          <CollapseBlock
+            expanded={this.state.collapse2Expanded}
+            bgColor={"#5f84dd"}
+            onClick={this.toggleActive2}
+          >
+            <h1>i enjoy clean code and creative interface</h1>
+            <p>{randomWords({ exactly: 1000, join: " " })}</p>
+          </CollapseBlock>
+          <CollapseBlock
+            expanded={this.state.collapse3Expanded}
+            bgColor={"#ff9b9e"}
+            onClick={this.toggleActive3}
+          >
+            {/* <TonePlayer /> */}
+            <h1>{randomWords({ exactly: 5, join: " " })}</h1>
+            <p>{randomWords({ exactly: 1000, join: " " })}</p>
+          </CollapseBlock>
+          <CollapseBlock
+            expanded={this.state.collapse4Expanded}
+            bgColor={"#46546e"}
+            onClick={this.toggleActive4}
+          >
+            <h1>?????</h1>
+            <p>{randomWords({ exactly: 1000, join: " " })}</p>
+          </CollapseBlock>
+        </CollapseContainer>
+      </div>
+    );
+  }
+}
--- src/components/TonePlayer.js
+++ src/components/TonePlayer.js
@@ -1,28 +1,28 @@
 import React, { Component } from "react";
-import styled, { css } from "styled-components";
-import Tone from "tone";
+import styled from "styled-components";
+// import Tone from "tone";
 import "../styles/App.css";
 
-import bass from "../audio/bass.mp3";
-import drums from "../audio/Drums.mp3";
-import keys1 from "../audio/keys1.mp3";
-import keys2 from "../audio/keys2.mp3";
-import arp from "../audio/arp.mp3";
+// import bass from "../audio/bass.mp3";
+// import drums from "../audio/Drums.mp3";
+// import keys1 from "../audio/keys1.mp3";
+// import keys2 from "../audio/keys2.mp3";
+// import arp from "../audio/arp.mp3";
 
-const synthA = new Tone.Synth({
-  oscillator: {
-    type: "fmsquare",
-    modulationType: "sawtooth",
-    modulationIndex: 3,
-    harmonicity: 6.7
-  },
-  envelope: {
-    attack: 0.001,
-    decay: 0.1,
-    sustain: 0.1,
-    release: 0.1
-  }
-}).toMaster();
+// const synthA = new Tone.Synth({
+//   oscillator: {
+//     type: "fmsquare",
+//     modulationType: "sawtooth",
+//     modulationIndex: 3,
+//     harmonicity: 6.7
+//   },
+//   envelope: {
+//     attack: 0.001,
+//     decay: 0.1,
+//     sustain: 0.1,
+//     release: 0.1
+//   }
+// }).toMaster();
 
 // const samples = {
 //   C2: bass,
@@ -34,10 +34,10 @@ const synthA = new Tone.Synth({
 
 // const samplerA = new Tone.Sampler(samples).toMaster();
 
-const player = new Tone.Player({
-  url: "../audio/Drums.mp3",
-  loop: true
-}).toMaster();
+// const player = new Tone.Player({
+//   url: "../audio/Drums.mp3",
+//   loop: true
+// }).toMaster();
 
 export default class TonePlayer extends Component {
   constructor(props) {
@@ -67,16 +67,17 @@ export default class TonePlayer extends Component {
   };
 
   playSample = () => {
-    player.start();
+    // player.start();
+    console.log("Sample should play on click");
   };
 
   componentDidMount() {
-    this.synthConfig();
-    var buffer = new Tone.Buffer("path/to/sound.mp3", function() {
-      //the buffer is now available.
-      buffer.get();
-    });
-    window.addEventListener("mousedown", this.playSample);
+    // // this.synthConfig();
+    // var buffer = new Tone.Buffer("path/to/sound.mp3", function() {
+    //   //the buffer is now available.
+    //   buffer.get();
+    // });
+    // window.addEventListener("mousedown", this.playSample);
   }
 
   render() {
@@ -88,7 +89,6 @@ export default class TonePlayer extends Component {
       width: 55%;
       margin: auto;
       padding-top: 2rem;
-      border: 2px dotted purple;
     `;
 
     const GridItem = styled.div`
